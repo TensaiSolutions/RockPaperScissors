0910158539d030ac444ec65b57a8d566351cf608
Tensai Solutions <sidellp@gmail.com>
2023-10-16 14:25:35 -0400

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,18 @@
+//
+// RockPaperScissorsApp.swift
+// RockPaperScissors
+//
+// Created by Tensai Solutions on 10/16/23
+// Copyright © 2023 Tensai Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
